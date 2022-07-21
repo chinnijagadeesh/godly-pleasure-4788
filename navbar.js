82560d6cbc7f7cbb0f736f5380ff53fb32ebfdbc
Akshay Rane <akshayrane848@gmail.com>
2022-07-21 15:28:05 +0530

    add file

---
+++ navbar.js
@@ -0,0 +1,14 @@
+let navbar=()=>{
+    return ` <img style="margin-right: -15px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB-CaH3KKIaa3GqUtNGnXVc20R82EkArJv0tXSMqG5dQ&s"
+    alt="">
+<p>Everhour</p>
+<p>Tour</p>
+<p>Integrations</p>
+<p>Customers</p>
+<p>Pricing</p>
+<p>Demo</p>
+<p style="margin-left: 400px;">Log in</p>
+<button id="sing">sing up</button>`
+}
+
+export default navbar;
