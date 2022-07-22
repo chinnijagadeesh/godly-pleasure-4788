0ee57b0cfbb6d97193d39e7ecab8508b30115134
Anandgupta09070 <vanand09070gu@gmail.com>
2022-07-22 21:11:47 +0530

    demo file added

--- script/customer.js
+++
@@ -1,5 +0,0 @@
-import navbar from "../common/navbar.js"
-document.getElementById("navbar").innerHTML=navbar();
-
-import footer from "../common/footer.js";
-document.getElementById("footer").innerHTML=footer();
